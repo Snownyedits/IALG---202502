722fd2962d8a23dda28ffba0ea57d42cff448546
Snownyedtis <pablomartinsyg@gmail.com>
2025-11-24 13:40:31 -0300

    odeio minha cognição

---
+++ semana11/questão01.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct Ponto{
+    int x;
+    int y;
+};
+
+struct Retangulo{
+    int v1;
+    int v2;
+};
+
+int main(){
+    Retangulo ret;
+    int contador = 0;
+    int pontos;
+    int total;
+    for(int i = 0; i < 2; i++){
+        cin >> pontos[i];
+    }
+    cin >> total;
+    while(contador < 4){
+        for(int i = 0; i < 2; i++){
+            cin >> ret.v1.x[i] >> ret.v1.y[i];
+        }
+        for(int i = 0; i < 2; i++){
+            cin >> ret.v2.x[i] >> ret.v2.y[i];
+        }
+        contador++;
+    }
+
+
+}
---
+++ semana11/questão02.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct Pessoas{
+    string nome;
+    int dia;
+    int mes;
+    int ano;
+};
+
+
+
+int main(){
+    int total;
+    cin >> total;
+
+    Pessoas pessoa[total];
+    string novo;
+    for(int i = 0; i < total; i++){
+        cin >> pessoa[i].nome >> pessoa[i].dia >> pessoa[i].mes >> pessoa[i].ano; 
+        novo = pessoa[i].dia + ;
+        if(pessoa[i].dia > novo)
+
+}
